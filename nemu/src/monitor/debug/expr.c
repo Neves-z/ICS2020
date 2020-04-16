0b3e64c8418bdc8b707642fb97ce8025debf833d
tracer-ics2017 <tracer@njuics.org>
2020-04-16 18:30:58 +0800

    >  compile 011810402 zhangjiahui Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  18:30:58 up  8:11,  2 users,  load average: 0.09, 0.07, 0.03 bce34b8e197f6e84fe5e3a66926bdb193aa7c43d

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -200,7 +200,7 @@ int op_comparative(int i){ //获取操作符的优先级
 int find_dominated_op(int p,int q){
   int stack[50]; // 用数组模拟栈,存储操作符在数组tokens中的位置;
   int i=0;
-  stack[0]=0;    // 注意：第一个位置绝对不会是'(';
+  stack[0]=p;    // 注意：第一个位置绝对不会是'(';
   for(int j=p;j<=q;j++){
    if(isoperand(j)&&tokens[stack[i]].type!='(') {  // 是操作符或括号且不在括号内部
       if(i==0){
