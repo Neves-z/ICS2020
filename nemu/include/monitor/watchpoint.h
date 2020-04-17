4e3fee0a0a24070336384fbc1e8b76842785ccda
tracer-ics2017 <tracer@njuics.org>
2020-04-17 22:33:09 +0800

    >  compile 011810402 zhangjiahui Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  22:33:09 up  3:49,  2 users,  load average: 0.00, 0.00, 0.00 52f2e42bff2ae8cedfe6ca9357c84eabe08794

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -6,10 +6,17 @@
 typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
-
+  char exprs[42];           // 存储需要监视表达式
+  int new_val;              // 储存表达式新值
+  int old_val;              // 储存表达式旧值
   /* TODO: Add more members if necessary */
 
 
 } WP;
+
+
+
+WP* new_wp();
+int free_wp(int NO);
 
 #endif
