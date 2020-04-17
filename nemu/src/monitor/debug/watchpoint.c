29bdfe675f5909e0fa0c1d505e9647eabbddf478
tracer-ics2017 <tracer@njuics.org>
2020-04-17 22:26:20 +0800

    >  compile 011810402 zhangjiahui Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  22:26:20 up  3:42,  2 users,  load average: 0.00, 0.00, 0.00 c4a902f6269d7886b744cd2ff3020fb7c49a735f

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -6,6 +6,71 @@
 static WP wp_pool[NR_WP];
 static WP *head, *free_;
 
+int init=0; // 记录已被使用的监视点数量
+
+void init_wp_pool();
+
+WP* new_wp(){ //启用一个空闲的监视点
+   if(init==0){
+     init_wp_pool();
+   }
+   WP* p=free_;
+   if(p){  //还有没有被使用的监视点结构
+     if(free_->next==head)  //置空free_,即监视点已被全部使用
+        free_=NULL;
+      else
+       free_=free_->next;
+     p->next=head;
+     head=p;
+     init++;//被使用的监视点+1
+     return p;
+   }
+   else{
+     printf("监视点已被全部使用！\n");
+     assert(0);
+   }
+
+}
+
+int free_wp(int NO){  //释放一个空闲的监视点
+    WP* p=head;
+    WP* pre=head;
+    if(head==NULL){
+      printf("没有正在使用的监视点！\n");
+      return 0;
+    }
+    else if(head->NO==NO){ //头结点就是这个监视点
+      if(head->next==free_){
+        head=NULL;   //置空head,即监视点全部空闲
+      }
+      else{
+        head=head->next;
+      }
+      p->next=free_;   //头插法
+      free_=p;
+      init--;  // 被使用的监视点数-1
+      printf("Watchpoint %d has been deleted！ \n",NO);
+      return 0;
+    }
+    else{
+       while(p->NO!=NO&&p->next){
+         pre=p;
+         p=p->next;
+       }
+       if(p->NO==NO){
+         pre->next=p->next;
+         p->next=free_;
+         free_=p;
+         init--; // 被使用的监视点数-1
+         printf("Watchpoint %d has been deleted！ \n",NO);
+       }
+       else{
+         printf("No  watchpoint %d\n!",NO);
+       }
+    }
+    return 0;
+}
+
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
