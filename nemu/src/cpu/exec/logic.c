8d4b10d5a46c640be9548c887b4ef22c92b76eb4
tracer-ics2017 <tracer@njuics.org>
2020-05-29 20:24:15 +0800

    >  compile 011810402 zhangjiahui Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  20:24:15 up  1:15,  2 users,  load average: 0.00, 0.00, 0.00 85f19a25fdf18ea2b9b0af0b8475f9f7cfb66b99

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -7,8 +7,13 @@ make_EHelper(test) {
 }
 
 make_EHelper(and) {
-  TODO();
-
+ // TODO();
+  rtl_and(&t1, &id_dest->val, &id_src->val); //目的操作数与源操作数相与
+  operand_write(id_dest, &t1); //写入目的操作数
+  rtl_update_ZFSF(&t0, id_dest->width); //更新ZFSF位
+  t0 = 0;
+  rtl_set_OF(&t0); //设置OF位为0
+  rtl_set_CF(&t0); //设置CF位为0
   print_asm_template2(and);
 }
 
