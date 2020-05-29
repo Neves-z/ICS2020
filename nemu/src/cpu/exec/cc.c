ee4245aa94d2c895cb7eadc3bfd9cd2fe4428459
tracer-ics2017 <tracer@njuics.org>
2020-05-29 21:27:46 +0800

    >  compile 011810402 zhangjiahui Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  21:27:46 up  2:18,  2 users,  load average: 0.00, 0.00, 0.00 bc88e810713725ae6fc62c9e3f46829289fd9d77

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,14 +14,36 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
-    case CC_O:
-    case CC_B:
-    case CC_E:
-    case CC_BE:
-    case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+    case CC_O://0
+      rtl_get_OF(dest);
+      break;
+    case CC_B://2
+      rtl_get_CF(dest); //小于，通过CF来判断不够减
+      break;
+    case CC_E://4
+      rtl_get_ZF(dest);
+      break;
+    case CC_BE: { //6
+      rtl_get_CF(&t0);
+      rtl_get_ZF(&t1);
+      rtl_or(dest, &t0, &t1); //小于等于，CF和ZF至少一个要等于1才行
+    }break;
+    case CC_S: //8
+      rtl_get_SF(dest);
+      break;
+    case CC_L: { //12
+      rtl_get_SF(&t0);
+      rtl_get_OF(&t1);
+      rtl_xor(dest, &t1, &t0); //带符号数的小于，SF不能等于OF
+    }break;
+    case CC_LE: { //14
+      rtl_get_ZF(&t0);
+      rtl_get_SF(&t1);
+      rtl_get_OF(&t2);
+      rtl_xor(&t3, &t1, &t2);
+      rtl_or(dest, &t0, &t3); //带符号数的小于等于，ZF=1或者SF不等于OF
+    }break;
+     // TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
