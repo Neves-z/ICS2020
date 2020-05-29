20a8ac76b4e2814de0ef99c09ecb1d782fa626e6
tracer-ics2017 <tracer@njuics.org>
2020-05-30 02:25:59 +0800

    >  compile 011810402 zhangjiahui Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  02:25:59 up  7:16,  2 users,  load average: 0.00, 0.00, 0.00 2b1852d25749baf5a8dd81b1adb3112ce1685b85

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -51,7 +51,7 @@ make_EHelper(sar) {
 }
 
 make_EHelper(shl) {
-  TODO();
+ // TODO();
   // unnecessary to update CF and OF in NEMU
   rtl_shl(&id_dest->val, &id_dest->val, &id_src->val);
   operand_write(id_dest, &id_dest->val);
@@ -60,7 +60,7 @@ make_EHelper(shl) {
 }
 
 make_EHelper(shr) {
-  TODO();
+ // TODO();
   // unnecessary to update CF and OF in NEMU
   rtl_shr(&id_dest->val, &id_dest->val, &id_src->val);
   operand_write(id_dest, &id_dest->val);
