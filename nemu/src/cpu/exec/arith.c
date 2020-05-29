17b7d90ae7e2952afd589175ed2d78d15be07903
tracer-ics2017 <tracer@njuics.org>
2020-05-30 04:41:50 +0800

    >  compile 011810402 zhangjiahui Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  04:41:50 up  9:32,  2 users,  load average: 0.01, 0.03, 0.00 3e8ad170ecc1976630283d719ae3e8175a6dbd50

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -73,7 +73,7 @@ make_EHelper(inc) {
 
 make_EHelper(dec) {
   //TODO();
-  rtl_subi(&t0,&id_dest->val,1);
+ /* rtl_subi(&t0,&id_dest->val,1);
   operand_write(id_dest,&t0);
   rtl_update_ZFSF(&t0,id_dest->width);
 
@@ -81,7 +81,17 @@ make_EHelper(dec) {
   //rtl_xor(&s0,&id_dest->val,&s0);
   //rtl_and(&s1,&s1,&s0);
   rtl_msb(&t1,&t1,id_dest->width);
-  rtl_set_OF(&t1);
+  rtl_set_OF(&t1);*/
+  rtl_subi(&t2, &id_dest->val, 1);
+  operand_write(id_dest, &t2);
+  rtl_update_ZFSF(&t2, id_dest->width);
+
+
+  rtl_xor(&t0, &id_dest->val, &id_src->val);
+  rtl_xor(&t1, &id_dest->val, &t2);
+  rtl_and(&t0, &t0, &t1);
+  rtl_msb(&t0, &t0, id_dest->width);
+  rtl_set_OF(&t0);
   print_asm_template1(dec);
 }
 
