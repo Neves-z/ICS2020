955a071670825d571b06f7194b049d9076bc5091
tracer-ics2017 <tracer@njuics.org>
2020-06-27 15:23:58 +0800

    >  compile 011810402 zhangjiahui Linux debian 4.19.0-8-686 #1 SMP Debian 4.19.98-1 (2020-01-26) i686 GNU/Linux  15:23:58 up  5:47,  2 users,  load average: 0.18, 0.07, 0.02 3fec02e7c8f2f3aaee25ba554096557c62a0f36

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -2,6 +2,7 @@
 #define __REG_H__
 
 #include "common.h"
+#include "memory/mmu.h"
 
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
@@ -43,11 +44,14 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
               };
             uint32_t value;//赋初值
           }eflags;
-      rtlreg_t cs;//cs寄存器
+       rtlreg_t cs;//cs寄存器
        struct {
          uint32_t base; //32位base
          uint16_t limit; //16位limit
-        }idtr;   
+        }idtr;
+      
+       CR0 cr0;
+       CR3 cr3;   
       };
    };
 }CPU_state;
--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,6 +12,39 @@ uint8_t pmem[PMEM_SIZE];
 
 /* Memory accessing interfaces */
 
+paddr_t page_translate(vaddr_t vaddr,bool write){
+
+    PDE pde;
+    PTE pte;
+    
+     
+    uint32_t DIR = (vaddr >> 22);  // 取虚拟地址中隐含的页目录项
+    uint32_t PDE_addr=(cpu.cr3.page_directory_base << 12) + (DIR << 2);  // 计算页目录项地址
+     
+    pde.val=paddr_read(PDE_addr,4);  // 取页表项基地址
+     Log("PDE_addr: %#x    pde.val: %#x",PDE_addr,pde.val);
+    assert(pde.present);  
+    
+    uint32_t PAGE=((vaddr >> 12) & 0x3ff); // 取虚拟地址的中间十位，即页表项内偏移地址；
+    uint32_t PTE_addr=(pde.val & 0xfffff000)+(PAGE << 2); //取从页目录里获取的页表项基址的高二十位与虚拟地址的中间十位*4结合
+    pte.val=paddr_read(PTE_addr,4);
+    Log("PTE_addr: %#x    pte.val: %#x",PTE_addr,pte.val);
+    assert(pte.present);
+    
+    uint32_t physical_addr=(pte.val & 0xfffff000)+(vaddr & 0xfff); // 物理地址就是pte的高二十位和虚拟地址的低十二位的结合；
+    Log("physical_addr: %#x",physical_addr);
+    pde.accessed=1;
+    paddr_write(PDE_addr,4,pde.val); //回写页目录项
+
+    if(pte.accessed==0 || (pte.dirty==0 && write)){
+      pte.accessed=1;
+      pte.dirty=1;
+    } 
+    paddr_write(PTE_addr,4,pte.val); //回写页表项
+   
+    return physical_addr;
+}
+
 uint32_t paddr_read(paddr_t addr, int len) {
   int mmio_n;
   if ((mmio_n = is_mmio(addr)) != -1)   //物理地址被映射到 I/O 空间
@@ -29,9 +62,31 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+  if(cpu.cr0.paging) {
+        if (((addr<<20)>>20)+len>0x1000) {  // 页内偏移量+len>4KB=2^12=0x1000
+            /* this is a special case, you can handle it later. */
+            assert(0);
+        }
+        else {
+            paddr_t paddr = page_translate(addr,false);
+            return paddr_read(paddr, len);
+        }
+    }
+  else
+      return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+  if(cpu.cr0.paging) {
+    if (((addr<<20)>>20)+len>0x1000) {  // 页内偏移量+len>4KB=2^12=0x1000
+        /* this is a special case, you can handle it later. */
+        assert(0);
+    }
+    else {
+        paddr_t paddr = page_translate(addr,true);
+        paddr_write(paddr, len, data);
+    }
+  }
+  else
+    paddr_write(addr, len, data);
 }
